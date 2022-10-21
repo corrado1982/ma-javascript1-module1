99dba225e0c082ef0bab55f2157e94c85b1ab26e
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-10-21 15:19:11 +0200

    d

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ const cats = [
     }
 ];
 
+//Question 1
+
+const cat = {
+    complain: function complainVerse() {
+        console.log("Meow");
+    }
+}
+cat.complain();
+
+//Question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+//Question 3
+
+heading.style.fontSize = "2em";
+
+//Question 4
